74aa2d28f108a8276ce7fc74792ed80a1a55d1d2
avsnaelfrb <annabhaniy@gmail.com>
2026-06-25 22:18:01 +0800

    put in agent runtime in try except block

--- main.py
+++ main.py
@@ -73,73 +73,78 @@ class Agent:
         self.llm = llm
 
 def main():
-    tools = AgentTools()
-    available_functions = {"list_vault": tools.list_vault, "search_file": tools.search_file}
-    agent_tool = Agent(
-        temp=0.1,
-        system_prompt="you are a tool calling agent",
-        thingking_mode=True,
-        stream_mode=True,
-        tools=[tools.list_vault, tools.search_file],
-        llm="qwen3.5:2b",
-    )
-    print("-"*64)
-    user_chat = str(input("Send question :> "))
-    print("-"*64, "\n")
-
-    messages = Conversation()
-    messages.add_user(user_chat)
-    # print(messages.messages)
-
-    response = OllamaClient().generate(agent=agent_tool, conversation=messages)
-
-    # result = response.message # no stream
-    result = StreamAccumulator()
-    for part in response:
-        result.append_iter(chunk=part)
-
-    messages.add_assistant(content=str(result.content), tool_calls=result.tool_calls)
-
-    for tool in result.tool_calls:
-        if function_to_calls := available_functions.get(tool.function.name):
-            output_tool = function_to_calls(**tool.function.arguments)
-            messages.add_tool(content=str(output_tool), func_name=tool.function.name, func_arg=tool.function.arguments)
+    while True:
+        try:
+            tools = AgentTools()
+            available_functions = {"list_vault": tools.list_vault, "search_file": tools.search_file}
+            agent_tool = Agent(
+                temp=0.1,
+                system_prompt="you are a tool calling agent",
+                thingking_mode=False,
+                stream_mode=True,
+                tools=[tools.list_vault, tools.search_file],
+                llm="qwen3.5:2b",
+            )
+            messages = Conversation()
             
-    if any(msg.get("role") == "tool" for msg in messages.messages):
-        print("\n")
-        print("-" * 20, "Sending back to agent", "-" * 20, "\n")
-        agent_response = Agent(
-            temp=0.2,
-            system_prompt="you are a helpfull assistant",
-            thingking_mode=False,
-            stream_mode=True,
-            llm="qwen3.5:2b",
-        )
-        res = OllamaClient().generate(agent=agent_response, conversation=messages)
-        final_result = StreamAccumulator()
-        for part in res:
-            final_result.append_iter(chunk=part)
-        messages.add_assistant(content=final_result.content, tool_calls=final_result.tool_calls) 
-            # if part.message.thinking:
-            #     print(part.message.thinking, end="", flush=True)
-            # if part.message.content:
-            #     if not done_thinking:
-            #         print("\n\n-------------- Final result --------------\n")
-            #         done_thinking = True
-            #     print(part.message.content, end="", flush=True)
-            # if part.message.tool_calls:
-            #     print("")
-            #     print("Model returned tool calls")
-            #     print(part.message.tool_calls)
-                
-        # print("\n", "=" * 20, "final messages", "=" * 20)
-        # print(messages.messages)
-        print("\n\n------------------- Final Conversation ------------------- \n")
-        print(messages.messages)
-        print("\nPanjang conversation: ",len(messages.messages))
-    else:
-        print("No tool calls returned")
-
+            print("-"*64)
+            user_chat = str(input("Send question :> "))
+            print("-"*64, "\n")
+        
+            messages.add_user(user_chat)
+            # print(messages.messages)
+        
+            response = OllamaClient().generate(agent=agent_tool, conversation=messages)
+        
+            # result = response.message # no stream
+            result = StreamAccumulator()
+            for part in response:
+                result.append_iter(chunk=part)
+        
+            messages.add_assistant(content=str(result.content), tool_calls=result.tool_calls)
+        
+            for tool in result.tool_calls:
+                if function_to_calls := available_functions.get(tool.function.name):
+                    output_tool = function_to_calls(**tool.function.arguments)
+                    messages.add_tool(content=str(output_tool), func_name=tool.function.name, func_arg=tool.function.arguments)
+                    
+            if any(msg.get("role") == "tool" for msg in messages.messages):
+                print("\n")
+                print("-" * 20, "Sending back to agent", "-" * 20, "\n")
+                agent_response = Agent(
+                    temp=0.2,
+                    system_prompt="You are a response assistant, answer with simple sentences.",
+                    thingking_mode=False,
+                    stream_mode=True,
+                    llm="qwen3.5:0.8b",
+                )
+                res = OllamaClient().generate(agent=agent_response, conversation=messages)
+                final_result = StreamAccumulator()
+                for part in res:
+                    final_result.append_iter(chunk=part)
+                messages.add_assistant(content=final_result.content, tool_calls=final_result.tool_calls) 
+                    # if part.message.thinking:
+                    #     print(part.message.thinking, end="", flush=True)
+                    # if part.message.content:
+                    #     if not done_thinking:
+                    #         print("\n\n-------------- Final result --------------\n")
+                    #         done_thinking = True
+                    #     print(part.message.content, end="", flush=True)
+                    # if part.message.tool_calls:
+                    #     print("")
+                    #     print("Model returned tool calls")
+                    #     print(part.message.tool_calls)
+                        
+                # print("\n", "=" * 20, "final messages", "=" * 20)
+                # print(messages.messages)
+                print("\n\n------------------- Final Conversation ------------------- \n")
+                print(messages.messages)
+                print("\nPanjang conversation: ",len(messages.messages))
+            else:
+                print("No tool calls returned")
+        except KeyboardInterrupt:
+            print("\n\nbyee -----")
+            break
 
 if __name__ == "__main__":
     main()
