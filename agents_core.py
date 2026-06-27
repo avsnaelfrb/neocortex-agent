6f50231fe453ecb408f0117fb4abb8ccd48cd91d
avsnaelfrb <annabhaniy@gmail.com>
2026-06-27 21:38:47 +0800

    move configuration core agent to separated files

---
+++ agents_core.py
@@ -0,0 +1,24 @@
+from typing import Any, Callable, Literal, Mapping, Optional, Sequence, Union
+
+from ollama import Tool
+from pydantic.json_schema import JsonSchemaValue
+
+
+class Agent:
+    def __init__(
+        self,
+        temp: float,
+        system_prompt: str,
+        thingking_mode: bool,
+        stream_mode: bool,
+        tools: Optional[Sequence[Union[Mapping[str, Any], Tool, Callable]]] = None,
+        format: Optional[Union[Literal["", "json"], JsonSchemaValue]] = None,
+        llm: str = "lfm2.5:latest",
+    ) -> None:
+        self.temp = temp
+        self.system_prompt = system_prompt
+        self.thinking_mode = thingking_mode
+        self.stream_mode = stream_mode
+        self.tools = tools
+        self.format: JsonSchemaValue | Literal["", "json"] | None = format
+        self.llm = llm
